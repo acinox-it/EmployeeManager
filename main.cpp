8c7bbef7064a2e57345f45a8a7ec3c9a164ec9e6
acinox-it <sahraoui.abdeldjalil@outlook.com>
2025-11-09 23:58:22 +0100

    Test class Employe

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include "Person.h"
+#include "Employe.h"
 
 
 using namespace std;
 
 int main()
 {
-    Person* loui = new Person("loui","sahraoui",21);
-    loui->display();
+    //Person louiBase("loui","sahraoui",21); without keyword const
+    //louiBase->display();
+    //Employe* louiEmployee = new Employe(louiBase, "Manager", 2550.0, 2023);
+    //louiEmployee->display();
+
 
     Person* rahaf = new Person("rahaf","sahraoui",8);
     rahaf->display();
 
-    delete loui;
+    Employe* loui = new Employe(Person("loui","sahraoui",21), "Manager", 2550.0, 2023);
+    loui->display();
+
+
+
     delete rahaf;
+    delete loui;
 
 
     return 0;
